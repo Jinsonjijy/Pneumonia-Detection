d90e57c3d54e468e53d64aa71b4eb6447475d0a2
jinson jijy <jinsonjijy50@gmail.com>
2025-06-19 22:19:12 +0530

    convert the bgr to rgb and show and plotted using the matplotlib

--- organize_train.py
+++ organize_train.py
@@ -8,7 +8,7 @@ viral_folder="chest_xray/train/Viral"#this is for making the path of that in a v
 Path(bacterial_folder).mkdir(parents=True,exist_ok=True)
 Path(viral_folder).mkdir(parents=True,exist_ok=True) #this is for doing the Path will point to the lacation and also the mkdir create a folder in the location if it is not present that is why we use the paraent and exit_ok paramenter in the mkdir paramenters
 for filename in os.listdir(source_folder):
-    src_path=os.path.join(source_folder,filename)
+    src_path=os.path.join(source_folder,filename)# this is joining or forming the locations means joingin the viral with the pnuemonica or bacterial folder
     if "bacteria" in filename:
         shutil.copy(src_path,os.path.join(bacterial_folder,filename))
     elif "virus" in filename:
